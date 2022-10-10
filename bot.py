10c941b33e40553f7f47d44ffee5610bd4344c5c
buhovets <x10c@duck.com>
2022-10-10 13:19:42 +0300

    getting user_id and chat_id

--- bot.py
+++ bot.py
@@ -1,1 +1,23 @@
+import logging
+import config
 
+from aiogram import Bot, Dispatcher, executor, types
+
+
+API_TOKEN = config.SECRET_KEY
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    user_id = message.get_args()
+    chat_id = message.chat.id
+    await message.reply(f"User_id: {user_id}\nChat_id: {chat_id}")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
